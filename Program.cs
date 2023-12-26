5a03046a4e12943fc97e983531ab0a014c44d24b
Marchenkov Sergei <serguei1978@mail.ru>
2023-12-26 22:13:55 +0300

    задача 4 решена

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿{
+        Console.WriteLine("Введите натуральное число N:");
+
+        if (int.TryParse(Console.ReadLine(), out int n) && n > 0)
+        {
+            Console.WriteLine($"Цифры числа {n} через запятую: {GetDigitsSeparatedByComma(n)}");
+        }
+        else
+        {
+            Console.WriteLine("Некорректный ввод. Введите натуральное число.");
+        }
+    }
+
+    static string GetDigitsSeparatedByComma(int number)
+    {
+        string result = "";
+
+        while (number > 0)
+        {
+            int digit = number % 10;
+            result = digit + (result == "" ? "" : ", " + result);
+            number /= 10;
+        }
+
+        return result;
+    }    
