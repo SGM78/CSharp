6550f9c01d2c0aa0dca12479d8e249481d991130
Marchenkov Sergei <serguei1978@mail.ru>
2023-12-26 22:13:55 +0300

    задача 3 решена

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите целое число из отрезка [10, 99]:");
+    {
+            if (int.TryParse(Console.ReadLine(), out int number))
+        {
+            if (number >= 10 && number <= 99)
+            {
+                int maxDigit = GetMaxDigit(number);
+
+                Console.WriteLine($"Наибольшая цифра в числе {number} - {maxDigit}.");
+            }
+            else
+            {
+                Console.WriteLine("Число не находится в указанном диапазоне [10, 99].");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Некорректный ввод. Введите целое число.");
+        }
+    }
+
+    static int GetMaxDigit(int n)
+    {
+        int maxDigit = 0;
+
+        while (n > 0)
+        {
+            int digit = n % 10;
+            maxDigit = Math.Max(maxDigit, digit);
+            n /= 10;
+        }
+
+        return maxDigit;
+    }
