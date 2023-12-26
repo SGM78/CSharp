d23aa810f63a1d3c6a07208f5b4a7b97a338cf76
Marchenkov Sergei <serguei1978@mail.ru>
2023-12-26 22:13:55 +0300

    задача 2 решена

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿        Console.Write("Введите координаты точки (X и Y) через пробел: ");
+        String[] axis = Console.ReadLine().Split(' ');
+        int x = Convert.ToInt32(axis [0]);
+        int y = Convert.ToInt32(axis [1]);
+
+        if (x > 0 && y > 0) 
+        {
+            Console.WriteLine("Точка ({0}, {1}) находится в первой координатной четверти", x, y);
+        } 
+        else if (x < 0 && y > 0) 
+        {
+            Console.WriteLine("Точка ({0}, {1}) находится во второй координатной четверти", x, y);
+        } 
+        else if (x < 0 && y < 0) 
+        {
+            Console.WriteLine("Точка ({0}, {1}) находится в третьей координатной четверти", x, y);
+        } 
+        else if (x > 0 && y < 0) 
+        {
+            Console.WriteLine("Точка ({0}, {1}) находится в четвёртой координатной четверти", x, y);
+        }
+        else
+        {
+            Console.WriteLine("Точка находится на оси координат");
+        }
+
