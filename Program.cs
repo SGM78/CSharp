b9186f33bdd7b78505c6175944fa8fbaee19aa32
Marchenkov Sergei <serguei1978@mail.ru>
2023-12-26 22:13:55 +0300

    програмама которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ Console.Write("Введите число: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        if (num % 7 == 0 && num % 23 == 0)
+        {
+            Console.WriteLine("{0} кратно 7 и 23", num);
+        } 
+        else 
+        {
+            Console.WriteLine("{0} не кратно 7 и 23", num);
+        }    
